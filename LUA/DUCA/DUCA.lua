f2e07de8b518f747421a2cb83b0c2c6f0ee4a7fd
Luan Luciano <luanluciano@outlook.com>
2017-07-01 12:18:42 -0300

    Update DUCA.lua

--- LUA/DUCA/DUCA.lua
+++ LUA/DUCA/DUCA.lua
@@ -1,11 +1,11 @@
 -- DUCA EVENTO by luanluciano93
+
 DUCA = {
 	EVENT_MINUTES = 40,
 	TELEPORT_POSITION = {x = 32365, y = 32236, z = 7},
-	TELEPORT_ACTION = 47200,
 	STORAGE_TEAM = 27000,
-	TOTAL_PLAYERS = 27001,
-	TOTAL_PONTOS = 27003,
+	TOTAL_PONTOS = 27001,
+	TOTAL_PLAYERS = 27003, -- global storage
 	LEVEL_MIN = 80,
 	REWARD_FIRST = {2160, 10},
 	REWARD_SECOND = {2160, 5},
@@ -34,8 +34,7 @@ local conditioGreen = createConditionObject(CONDITION_OUTFIT)
 setConditionParam(conditioGreen, CONDITION_PARAM_TICKS, -1)
 addOutfitCondition(conditioGreen, {lookType = 134, lookHead = 101, lookBody = 101, lookLegs = 101, lookFeet = 101})
 
-
----------------------------------------------------------------------------------------------------------------
+-- function DUCA.teleportCheck()
 DUCA.teleportCheck = function()
 	local item = getTileItemById(DUCA.TELEPORT_POSITION, 1387).uid
 	if item > 0 then
@@ -44,17 +43,18 @@ DUCA.teleportCheck = function()
 
 		print(">>> Duca Event was finished. <<<")
 	else		
-		doBroadcastMessage("Titan Battle Event was started in Thais's Temple and will close in ".. DUCA.EVENT_MINUTES .." minutes.")
-		print(">>> Titan Battle Event was started. <<<")
+		doBroadcastMessage("Duca Event was started and will close in ".. DUCA.EVENT_MINUTES .." minutes.")
+		print(">>> Duca Event was started. <<<")
 
 		local teleport = doCreateItem(1387, 1, DUCA.TELEPORT_POSITION)
-		doItemSetAttribute(teleport, "aid", DUCA.TELEPORT_ACTION)
+		doItemSetAttribute(teleport, "aid", 48000)
 
 		setGlobalStorageValue(DUCA.TOTAL_PLAYERS, 0)
 		addEvent(DUCA.teleportCheck, DUCA.EVENT_MINUTES * 60 * 1000)
 	end
 end
----------------------------------------------------------------------------------------------------------------
+
+-- function DUCA.addPlayerinTeam(cid, team)
 DUCA.addPlayerinTeam = function(cid, team)
 	doRemoveCondition(cid, CONDITION_OUTFIT)
 	doRemoveCondition(cid, CONDITION_INVISIBLE)
@@ -72,7 +72,9 @@ DUCA.addPlayerinTeam = function(cid, team)
 	doCreatureAddHealth(cid, getCreatureMaxHealth(cid))
 	doCreatureAddMana(cid, getCreatureMaxMana(cid))
 end
----------------------------------------------------------------------------------------------------------------
+
+
+-- function DUCA.balanceTeam()
 DUCA.balanceTeam = function()
 	local time1, time2 = 0, 0
 	for _, cid in pairs(getPlayersOnline()) do
@@ -85,25 +87,26 @@ DUCA.balanceTeam = function()
 
 	return (time1 <= time2) and 1 or 2
 end
----------------------------------------------------------------------------------------------------------------
+
+-- function DUCA.removePlayer(cid)
 DUCA.removePlayer = function(cid)
-	local thais = {x = 32369, y = 32240, z = 5}
-	doTeleportThing(cid, thais)
 	doRemoveCondition(cid, CONDITION_OUTFIT)
-	doRemoveCondition(cid, CONDITION_HUNTING)
-	doRemoveCondition(cid, CONDITION_INFIGHT)
-	doRemoveCondition(cid, CONDITIONID_COMBAT)
-	doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You are dead in Titan Battle and your GBattle points is set to 0!")
+	--doRemoveCondition(cid, CONDITION_HUNTING)
+	--doRemoveCondition(cid, CONDITION_INFIGHT)
+	--doRemoveCondition(cid, CONDITIONID_COMBAT)
+	doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You are dead in Duca Event and your Duca points is set to 0!")
 	doCreatureAddHealth(cid, getCreatureMaxHealth(cid))
 	doCreatureAddMana(cid, getCreatureMaxMana(cid))	
 	setPlayerStorageValue(cid, DUCA.STORAGE_TEAM, 0)
 	setPlayerStorageValue(cid, DUCA.TOTAL_PONTOS, 0)
 	unregisterCreatureEvent(cid, "Duca-Death")
 	unregisterCreatureEvent(cid, "Duca-Combat")
+	doTeleportThing(cid, getTownTemplePosition(getPlayerTown(cid)))
 
 	setGlobalStorageValue(DUCA.TOTAL_PLAYERS, getGlobalStorageValue(DUCA.TOTAL_PLAYERS) - 1)
 end
----------------------------------------------------------------------------------------------------------------
+
+-- function DUCA.updateRank()
 DUCA.updateRank = function()
 	local participantes = {}
 	for _, uid in pairs(getPlayersOnline()) do
@@ -124,25 +127,23 @@ DUCA.updateRank = function()
 		DUCA.addPlayerinTeam(participantes[1], 4)
 	end
     
-    if (#participantes >= 11) then
-    	for i = 2, 11 do
+    	if (#participantes >= 11) then
+    		for i = 2, 11 do
 			DUCA.addPlayerinTeam(participantes[i], 3)
 		end
 	end
 end
----------------------------------------------------------------------------------------------------------------
+
+-- function DUCA.finishEvent()
 DUCA.finishEvent = function()
 	DUCA.updateRank()
 	for _, uid in pairs(getPlayersOnline()) do
 		if getPlayerStorageValue(uid, DUCA.STORAGE_TEAM) == 4 then
-			local winname = getCreatureName(uid)
-			doBroadcastMessage("Congratulation ".. winname .."!! The Titan Battle is finish. ".. winname .." win 25 golden tokens and a boost experience.")
-			doPlayerAddItem(uid, 6527, 25)
-			doPlayerAddItem(uid, 8981, 1)
+			local winner = getCreatureName(uid)
+			doBroadcastMessage("Congratulation ".. winner .."!! Duca Event is finish. ".. winner .." win reward.")
+			doPlayerAddItem(uid, DUCA.REWARD_FIRST[1], DUCA.REWARD_FIRST[2])
 		elseif getPlayerStorageValue(uid, DUCA.STORAGE_TEAM) == 3 then
-			doPlayerSendTextMessage(uid, MESSAGE_INFO_DESCR, "Congratulation!! You win 5 golden tokens and 50000 golds.")
 			doPlayerAddItem(uid, DUCA.REWARD_SECOND[1], DUCA.REWARD_SECOND[2])
-			doPlayerAddItem(uid, 6527, 5)
 		end
 
 		if getPlayerStorageValue(uid, DUCA.STORAGE_TEAM) > 0 then

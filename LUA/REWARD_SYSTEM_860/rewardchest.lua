a314ba91029bac7ba7752f1d36ba887fe4b6c3e1
Luan Luciano <luanluciano@outlook.com>
2017-08-05 10:26:29 -0300

    Create rewardchest.lua

---
+++ LUA/REWARD_SYSTEM_860/rewardchest.lua
@@ -0,0 +1,245 @@
+-- Sistema de recompensa criado por luanluciano93
+
+REWARDCHEST = {
+	rewardBagId = 2595,
+	formula = {hit = 3, block = 1, suport = 9},
+    storageExaust = 60000,
+	town_id = 1,
+	bosses = {
+		["ghazbaran"] =
+		{
+			comum = {
+				{2143, 10, 100}, -- white pearl
+				{2146, 10, 100}, -- small sapphire
+				{2145, 10, 100}, -- small diamond
+				{2144, 10, 100}, -- black pearl
+				{2149, 10, 100}, -- small emeralds
+				{5954, 3, 100}, -- demon horn
+				{7896, 1, 100}, -- glacier kilt
+				{7590, 1, 100}, -- great mana potion
+				{2179, 1, 100}, -- gold ring
+				{2151, 1, 100}, -- talon
+			}, 
+
+			semi_raro = {
+				{2152, 90, 100}, -- platinum coin
+				{7368, 50, 100}, -- assassin star
+				{2130, 1, 100}, -- golden amulet
+				{2436, 1, 100}, -- skull staff
+				{2447, 1, 100}, -- twin axe
+				{2171, 1, 100}, -- platinum amulet
+				{2158, 1, 100}, -- blue gem
+				{2393, 1, 100}, -- giant sword
+				{2164, 1, 100}, -- might ring
+				{7454, 1, 100}, -- glorious axe
+				{6553, 1, 100}, -- ruthless axe
+				{9971, 1, 100}, -- gold ingot
+			},
+
+			raro = {
+				{2160, 3, 100}, -- crystal coin
+				{8887, 1, 100}, -- frozen plate
+				{2470, 1, 100}, -- golden legs
+				{2472, 1, 100}, -- magic plate armor
+				{7431, 1, 100}, -- demonbone
+				{2520, 1, 100}, -- demon shield
+				{5943, 1, 100}, -- morgaroth hearth
+				{2466, 1, 100}, -- golden armor
+				{2514, 1, 100}, -- mastermind shield
+				{2142, 1, 100}, -- ancient amulet
+				{2123, 1, 100}, -- ring of the sky
+				{1984, 1, 100}, -- blue tome
+			},
+
+			muito_raro = {
+				{8866, 1, 100}, -- robe of the ice queen
+				{6553, 1, 100}, -- ruthless axe
+				{8884, 1, 100}, -- oceanborn leviathan armor
+				{2421, 1, 100}, -- thunder hammer
+				{2646, 1, 100}, -- golden boots
+				{7455, 1, 100}, -- mythril axe
+			},
+			
+			sempre = {
+				{2148, 90, 100}, -- gold coin
+				{6500, 9, 100}, -- demonic essence
+				{2177, 1, 100}, -- life crystal
+				{2214, 1, 100}, -- ring of healing
+			},
+			
+			storage = 65479, 
+		},
+		
+		["orshabaal"] =
+		{
+			comum = {
+				{2143, 10, 100}, -- white pearl
+				{2146, 10, 100}, -- small sapphire
+				{2145, 10, 100}, -- small diamond
+				{2144, 10, 100}, -- black pearl
+				{2149, 10, 100}, -- small emeralds
+				{5954, 3, 100}, -- demon horn
+				{7896, 1, 100}, -- glacier kilt
+				{2432, 1, 100}, -- fire axe
+				{2462, 1, 100}, -- devil helmet
+				{7590, 1, 100}, -- great mana potion
+				{2179, 1, 100}, -- gold ring
+				{2151, 1, 100}, -- talon
+			}, 
+
+			semi_raro = {
+				{2195, 1, 100}, -- boots of haste
+				{2436, 1, 100}, -- skull staff
+				{2393, 1, 100}, -- giant sword
+				{5954, 5, 100}, -- demon horn
+			},
+
+			raro = {
+				{2470, 1, 100}, -- golden legs
+				{2472, 1, 100}, -- magic plate armor
+				{2514, 1, 100}, -- mastermind shield
+				{2520, 1, 100}, -- demon shield
+				{1982, 1, 100}, -- purple tome
+				{2123, 1, 100}, -- ring of the sky
+			},
+
+			muito_raro = {
+				{8890, 1, 100}, -- Robe of the underworld
+				{2421, 1, 100}, -- thunder hammer
+			},
+			
+			sempre = {
+				{5808, 1, 100}, -- orshabaal's brain
+				{2171, 1, 100}, -- platinum amulet
+				{2148, 90, 100}, -- gold coin
+				{2146, 10, 100}, -- small sapphire
+			},
+			
+			storage = 65480, 
+		},
+		
+		["ferumbras"] =
+		{
+			comum = {
+				{2143, 10, 100}, -- white pearl
+				{2146, 10, 100}, -- small sapphire
+				{2145, 10, 100}, -- small diamond
+				{2144, 10, 100}, -- black pearl
+				{2149, 10, 100}, -- small emeralds
+				{7416, 1, 100}, -- Bloody Edge
+				{7896, 1, 100}, -- glacier kilt
+				{2432, 1, 100}, -- fire axe
+				{2462, 1, 100}, -- devil helmet
+				{7590, 1, 100}, -- great mana potion
+				{2179, 1, 100}, -- gold ring
+				{2151, 1, 100}, -- talon
+			}, 
+
+			semi_raro = {
+				{2195, 1, 100}, -- boots of haste
+				{2472, 1, 100}, -- magic plate armor
+				{2393, 1, 100}, -- giant sword
+				{2470, 1, 100}, -- golden legs
+				{2514, 1, 100}, -- mastermind shield
+			},
+
+			raro = {
+				{8885, 1, 100}, -- divine plate
+				{2520, 1, 100}, -- demon shield
+				{8930, 1, 100}, -- emerald sword
+				{2522, 1, 100}, -- great shield
+				{2421, 1, 100}, -- thunder hammer
+			},
+
+			muito_raro = {
+				{5903, 1, 100}, -- ferumbras hat
+			},
+			
+			sempre = {
+				{2171, 1, 100}, -- platinum amulet
+				{2148, 90, 100}, -- gold coin
+				{2146, 10, 100}, -- small sapphire
+			},
+			
+			storage = 65481, 
+		},
+		["zulazza the corruptor"] =
+		{
+			comum = {
+				{2158, 1, 100}, -- blue gem
+				{2156, 1, 100}, -- red gem
+				{2155, 1, 100}, -- green gem
+				{2154, 1, 100}, -- yellow gem
+				{2153, 1, 100}, -- violet gem
+			}, 
+
+			semi_raro = {
+				{5944, 5, 100}, -- soul orb
+			},
+
+			raro = {
+				{2514, 1, 100}, -- mastermind shield
+			},
+
+			muito_raro = {
+				{11114, 1, 100}, -- dragon scale boots
+				{8882, 1, 100}, -- earthborn titan armor
+			},
+			
+			sempre = {
+				{2152, 90, 100}, -- platinum coin
+				{9971, 5, 100}, -- gold ingot
+			},
+			
+			storage = 65482, 
+		},
+		["morgaroth"] =
+		{
+			comum = {
+				{2143, 10, 100}, -- white pearl
+				{2146, 10, 100}, -- small sapphire
+				{2145, 10, 100}, -- small diamond
+				{2144, 10, 100}, -- black pearl
+				{2149, 10, 100}, -- small emeralds
+				{5954, 3, 100}, -- demon horn
+				{7896, 1, 100}, -- glacier kilt
+				{2432, 1, 100}, -- fire axe
+				{2462, 1, 100}, -- devil helmet
+				{7590, 1, 100}, -- great mana potion
+				{2179, 1, 100}, -- gold ring
+				{2151, 1, 100}, -- talon
+			}, 
+
+			semi_raro = {
+				{2195, 1, 100}, -- boots of haste
+				{2393, 1, 100}, -- giant sword
+				{5954, 5, 100}, -- demon horn
+				{2123, 1, 100}, -- ring of the sky
+			},
+
+			raro = {
+				{8886, 1, 100}, -- molten plate
+				{2472, 1, 100}, -- magic plate armor
+				{8867, 1, 100}, -- dragon robe
+				{2514, 1, 100}, -- mastermind shield
+				{2520, 1, 100}, -- demon shield
+				{1982, 1, 100}, -- purple tome
+				{8851, 1, 100}, -- Royal Crossbow
+			},
+
+			muito_raro = {
+				{2421, 1, 100}, -- thunder hammer
+				{2522, 1, 100}, -- great shield
+				{8850, 1, 100}, -- chain bolter
+			},
+			
+			sempre = {
+				{5943, 1, 100}, -- morgaroth's brain
+				{2171, 1, 100}, -- platinum amulet
+				{2148, 90, 100}, -- gold coin
+				{2146, 10, 100}, -- small sapphire
+			},
+			storage = 65483, 
+		},
+	}
+}

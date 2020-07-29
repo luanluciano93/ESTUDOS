22600c7ab5afcac75ad372e98b042b4d9157e146
Luan Luciano <luanluciano@outlook.com>
2020-07-28 23:57:28 -0300

    Update talkactions.lua

--- LUA/Battlefield/talkactions.lua
+++ LUA/Battlefield/talkactions.lua
@@ -1,10 +1,12 @@
+-- <talkaction words="/battlefield" script="battlefield.lua" />
+
 dofile('data/lib/custom/battlefield.lua')
 
 function onSay(player, words, param)
 	if player:getGroup():getAccess() then
-		if BATTLEFIELD:totalPlayers() == 0 then
+		if battlefield_totalPlayers() == 0 then
 			eventsOutfit = {}
-			BATTLEFIELD:teleportCheck()
+			battlefield_teleportCheck()
 		else
 			player:sendCancelMessage("Battlefield event is already running.")
 		end

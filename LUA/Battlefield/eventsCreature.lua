58b46c7a3372c700766281bba4303db9e555fbc5
Luan Luciano <luanluciano@outlook.com>
2020-07-29 00:02:02 -0300

    Create eventsCreature.lua

---
+++ LUA/Battlefield/eventsCreature.lua
@@ -0,0 +1,23 @@
+dofile('data/lib/custom/battlefield.lua')
+
+function Creature:onChangeOutfit(outfit)
+	if self:isPlayer() then
+		-- Battlefield event
+		if self:getStorageValue(BATTLEFIELD.storage) > 0 then
+			self:sendCancelMessage("You can't change my outfit inside the event.")
+			return false
+		end
+	end
+	return true
+end
+
+IN ============= function Creature:onTargetCombat(target)
+
+	-- Battlefield event
+	if self:isPlayer() and target:isPlayer() then
+		if self:getStorageValue(BATTLEFIELD.storage) > 0 then
+			if self:getStorageValue(BATTLEFIELD.storage) == target:getStorageValue(BATTLEFIELD.storage) then
+				return RETURNVALUE_YOUMAYNOTATTACKTHISPLAYER
+			end
+		end
+	end

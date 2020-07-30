c05c5d1d0667d53e77f00afc6fb3deedd2fbfff8
Luan Luciano <luanluciano@outlook.com>
2020-07-29 23:38:17 -0300

    Update creaturescript.lua

--- LUA/Safezone/creaturescript.lua
+++ LUA/Safezone/creaturescript.lua
@@ -1,16 +1,18 @@
 -- <event type="login" name="Safezone-Login" script="events/safezone.lua"/>
 -- <event type="logout" name="Safezone-Logout" script="events/safezone.lua"/>
 
+dofile('data/lib/custom/safezone.lua')
+
 function onLogin(player)
-	if player:getStorageValue(Storage.safezoneEvent) > 0 then
-		player:setStorageValue(Storage.safezoneEvent, 0)
+	if player:getStorageValue(SAFEZONE.storage) > 0 then
+		player:setStorageValue(SAFEZONE.storage, 0)
 		player:teleportTo(player:getTown():getTemplePosition())
 	end
 	return true
 end
 
 function onLogout(player)
-	if player:getStorageValue(Storage.safezoneEvent) > 0 then
+	if player:getStorageValue(SAFEZONE.storage) > 0 then
 		player:sendCancelMessage("You can not logout in event!")
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
 		return false

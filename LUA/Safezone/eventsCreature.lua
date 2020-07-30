a15e3674fc1dffc43085507dc50ad132f7a1d7fd
Luan Luciano <luanluciano@outlook.com>
2020-07-29 23:43:07 -0300

    Update eventsCreature.lua

--- LUA/Safezone/eventsCreature.lua
+++ LUA/Safezone/eventsCreature.lua
@@ -4,7 +4,7 @@ dofile('data/lib/custom/safezone.lua')
 IN ============= function Creature:onChangeOutfit(outfit)
 	if self:isPlayer() then
 		-- Battlefield event
-		if self:getStorageValue(AFEZONE.storage) > 0 then
+		if self:getStorageValue(SAFEZONE.storage) > 0 then
 			self:sendCancelMessage("You can't change my outfit inside the event.")
 			return false
 		end

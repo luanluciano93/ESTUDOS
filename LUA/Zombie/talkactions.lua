5e70d9d4c86ab1e21c33e0918e66387dcdb63b6c
Luan Luciano <luanluciano@outlook.com>
2020-07-30 11:51:34 -0300

    Create talkactions.lua

---
+++ LUA/Zombie/talkactions.lua
@@ -0,0 +1,14 @@
+-- <talkaction words="/zombie" script="events/zombie.lua" />
+
+dofile('data/lib/custom/zombie.lua')
+
+function onSay(player, words, param)
+	if player:getGroup():getAccess() then
+		if zombie_totalPlayers() == 0 then
+			zombie_teleportCheck()
+		else
+			player:sendCancelMessage("Zombie event is already running.")
+		end
+	end
+	return false
+end

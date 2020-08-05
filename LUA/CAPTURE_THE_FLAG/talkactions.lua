301d87c8d68a5efb54dcb9dfcf5629130b0fb820
Luan Luciano <luanluciano@outlook.com>
2020-08-05 17:05:24 -0300

    Create talkactions.lua

---
+++ LUA/CAPTURE_THE_FLAG/talkactions.lua
@@ -0,0 +1,14 @@
+-- <talkaction words="/capturetheflag" script="capturetheflag.lua" />
+
+dofile('data/lib/custom/capturetheflag.lua')
+
+function onSay(player, words, param)
+	if player:getGroup():getAccess() then
+		if capturetheflag_totalPlayers() == 0 then
+			capturetheflag_teleportCheck()
+		else
+			player:sendCancelMessage("Capture the Flag event is already running.")
+		end
+	end
+	return false
+end

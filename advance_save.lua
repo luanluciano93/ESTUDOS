1d3a1a1e7bbd059cbd94247c1c50e61afe398fe9
Luan Luciano <luanluciano@outlook.com>
2021-12-10 14:51:53 -0300

    Create advance_save.lua

---
+++ advance_save.lua
@@ -0,0 +1,17 @@
+local creatureevent = CreatureEvent("AdvanceSave")
+function creatureevent.onAdvance(player, skill, oldLevel, newLevel)
+	if skill ~= SKILL_LEVEL or newLevel <= oldLevel then
+		return true
+	end
+
+	player:getPosition():sendMagicEffect(math.random(CONST_ME_FIREWORK_YELLOW, CONST_ME_FIREWORK_BLUE))
+
+	player:addHealth(player:getMaxHealth())
+	player:addMana(player:getMaxMana())
+
+	player:save()
+
+	return true
+end
+
+creatureevent:register()

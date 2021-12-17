293cb6cb01df970a8c0d8846c313435b6062c861
Luan Luciano <luanluciano@outlook.com>
2021-12-17 03:00:32 -0300

    Update aol.lua

--- aol.lua
+++ aol.lua
@@ -9,16 +9,17 @@ function onSay(player, words, param)
 	local itemType = ItemType(2173)
 	local itemWeight = itemType:getWeight()
 	local playerCap = player:getFreeCapacity()
-	if playerCap >= itemWeight then
-		if not player:removeTotalMoney(10000) then
-			player:sendCancelMessage("You don't have 10000 gold coins to buy an amulet of loss.")
-			player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		else
-			player:addItem(2173, 1)
-			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
-		end
-	else
+	if playerCap < itemWeight then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You have found a ' .. itemType:getName() .. ' weighing ' .. itemWeight .. ' oz it\'s too heavy.')
+		player:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
+	if player:removeTotalMoney(10000) then
+		player:addItem(2173, 1)
+		player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+	else
+		player:sendCancelMessage("You don't have 10000 gold coins to buy an amulet of loss.")
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
 	end
 

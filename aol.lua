3d0cf52f879ff80f6a9e2051bfbe7bf952d9dd55
Luan Luciano <luanluciano@outlook.com>
2021-12-17 02:52:03 -0300

    Create aol.lua

---
+++ aol.lua
@@ -0,0 +1,26 @@
+function onSay(player, words, param)
+
+	if not Tile(player:getPosition()):hasFlag(TILESTATE_PROTECTIONZONE) then
+		player:sendCancelMessage("To buy amulet of loss you need to be in protection zone.")
+		player:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
+	local itemType = ItemType(2173)
+	local itemWeight = itemType:getWeight()
+	local playerCap = player:getFreeCapacity()
+	if playerCap >= itemWeight then
+		if not player:removeTotalMoney(10000) then
+			player:sendCancelMessage("You don't have 10000 gold coins to buy an amulet of loss.")
+			player:getPosition():sendMagicEffect(CONST_ME_POFF)
+		else
+			player:addItem(2173, 1)
+			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+		end
+	else
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You have found a ' .. itemType:getName() .. ' weighing ' .. itemWeight .. ' oz it\'s too heavy.')
+		player:getPosition():sendMagicEffect(CONST_ME_POFF)
+	end
+
+	return false
+end

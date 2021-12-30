6460a274564113d6ab59318c45594ecdd71374a2
Luan Luciano <luanluciano@outlook.com>
2021-12-30 07:21:08 -0300

    Update guildpoints_talkaction.lua

--- LUA/guildshop/guildpoints_talkaction.lua
+++ LUA/guildshop/guildpoints_talkaction.lua
@@ -1,6 +1,14 @@
 -- <talkaction words="!guildpoints" separator=" " script="custom/guildpoints.lua"/>
 
 --[[ 
+
+ALTER TABLE `accounts` ADD `guild_points` INTEGER(11) NOT NULL DEFAULT '0';
+
+ALTER TABLE `accounts` ADD `guild_points_stats` INT NOT NULL DEFAULT '0';
+
+ALTER TABLE `guilds` ADD `last_execute_points` INT NOT NULL DEFAULT '0';
+
+
 CREATE TABLE IF NOT EXISTS `znote_shop` (
   `id` int NOT NULL AUTO_INCREMENT,
   `type` int NOT NULL,
@@ -10,7 +18,6 @@ CREATE TABLE IF NOT EXISTS `znote_shop` (
   `points` int NOT NULL DEFAULT '10',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
-
 informações sobre adesão dos pontos.
 ok -- Comando Utilizado:	!guildpoints (leader)
 ok -- Level Mínimo:	Level 70
@@ -18,9 +25,7 @@ ok -- Players On-line:	4 On-line
 ok -- IPs Diferentes:	2 IPs
 Quantidade de Pontos:	10 Pontos (cada player)
 ok -- O comando só pode ser executado uma vez por dia 
-
 e cada player só recebe uma vez por account, não adianta entra em outra guild e nem tentar com outro character.
-
 ]]--
 
 local config = {
@@ -60,22 +65,28 @@ function onSay(player, words, param)
 
 	local guild = player:getGuild()
 	if not guild then
+		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_RED, "[GUILD POINTS] You need to be in a guild in order to execute this command.")
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, 'You are not in any guild.')
-		return false
-	end
-
-	if player:getGuildLevel() ~= 3 then
-		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, 'Only guild leader can request points.')
 		return false
 	end
 
 	local guildId = guild:getId()
+	if not guildId then
+		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_RED, "[GUILD POINTS] You need to be in a guild in order to execute this command.")
+		player:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
+	if player:getGuildLevel() < 3 then
+		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_RED, '[GUILD POINTS] You need to be the guild leader to execute this command.')
+		player:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
 	local resultId = db.storeQuery('SELECT `last_execute_points` FROM `guilds` WHERE `id` = '.. guildId)
 	if not resultId then
+		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_RED, '[GUILD POINTS] The command can only be run once every '.. config.executeInterval ..' hours.')	
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		player:sendCancelMessage('Error while running database query.')
 		return false
 	end
 
@@ -83,14 +94,20 @@ function onSay(player, words, param)
 	result.free(resultId)
 
 	if lastExecution >= os.time() then
+		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_RED, '[GUILD POINTS] The command can only be run once every '.. config.executeInterval ..' hours.')
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, 'The command can only be run once every '.. config.executeInterval ..' hours.')
 		return false
 	end
 
 	local membersTable, validIpsTable = {}, {}
 	for _, member in ipairs(guild:getMembersOnline()) do
 		if member:getLevel() >= config.minimumLevel then
+
+			-- testar o "Game.getAccountStorageValue" se não tiver nada salvo, volta o ou -1 ou false?
+			-- Game.getAccountStorageValue(accountId, key)
+			-- Game.setAccountStorageValue(accountId, key, value)
+			
+			
 			local accountId = player:getAccountId()
 			local getAccountStorage = Game.getAccountStorageValue(accountId, config.accountStorage)
 			if getAccountStorage then
@@ -118,38 +135,29 @@ function onSay(player, words, param)
 
 	local totalMembrosOnline = #membersTable
 	if totalMembrosOnline < config.membersNeeded then
+		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_RED, '[GUILD POINTS] Only '.. totalMembrosOnline ..' guild members online, you need '.. config.membersNeeded ..' guild members with level '.. config.minimumLevel ..' or higher.')
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, 'Only '.. totalMembrosOnline ..' guild members online, you need '.. config.membersNeeded ..' guild members with level '.. config.minimumLevel ..' or higher.')
 		return false
 	end
 
 	local totalMembrosIps = #validIpsTable
 	if totalMembrosIps < config.minimumDifferentIps then
-		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, 'Only ' .. totalMembrosIps .. ' members are valid, you need ' ..config.minimumDifferentIps .. ' players with different ip addresses.')
+		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_RED, '[GUILD POINTS] Only ' .. totalMembrosIps .. ' members are valid, you need ' ..config.minimumDifferentIps .. ' players with different ip addresses.')
+		player:getPosition():sendMagicEffect(CONST_ME_POFF)		
 		return false
 	end
 
-CREATE TABLE IF NOT EXISTS `account_storage` (
-  `account_id` int NOT NULL,
-  `key` int unsigned NOT NULL,
-  `value` int NOT NULL,
-  PRIMARY KEY (`account_id`, `key`),
-  FOREIGN KEY (`account_id`) REFERENCES `accounts`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
-
-Game.getAccountStorageValue(accountId, key)
-Game.setAccountStorageValue(accountId, key, value)
-
-	local validAccounts = getValidAccounts(guild)
+	-- local validAccounts = getValidAccounts(guild)
 
 	db.query('UPDATE `guilds` SET `last_execute_points` = ' .. (os.time() + config.executeInterval * 3600) .. ' WHERE `guilds`.`id` = '.. guildId ..';')
+
 	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, #validAccounts .. ' guild members received points.')
+
 	if #validAccounts > 0 then
 		db.query('UPDATE `accounts` SET `guild_points` = `guild_points` + ' .. config.pointAmount .. ', `guild_points_stats` = ' .. os.time() .. ' WHERE `id` IN (' .. table.concat(validAccounts, ',') .. ');')
-		for i = 1, #members do
-			local member = members[i]
-			if isInArray(validAccounts, member:getAccountId()) then
+		for i = 1, #membersTable do
+			local member = membersTable[i]
+			if table.contains(validAccounts, member:getAccountId()) then
 				member:sendTextMessage(MESSAGE_INFO_DESCR, 'You received ' .. config.pointAmount .. ' guild points.')
 			end
 		end

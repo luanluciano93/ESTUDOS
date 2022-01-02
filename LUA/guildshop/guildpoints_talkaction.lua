67a0b15a8f19a0c55154d62693590b8d2babda07
Luan Luciano <luanluciano@outlook.com>
2022-01-01 23:42:06 -0300

    Update guildpoints_talkaction.lua

--- LUA/guildshop/guildpoints_talkaction.lua
+++ LUA/guildshop/guildpoints_talkaction.lua
@@ -1,15 +1,13 @@
--- <talkaction words="!guildpoints" separator=" " script="custom/guildpoints.lua"/>
-
 -- ALTER TABLE `guilds` ADD `last_execute_points` int NOT NULL DEFAULT '0';
 -- ALTER TABLE `znote_accounts` ADD `guild_points` smallint unsigned NOT NULL DEFAULT '0';
 
 local config = {
 	executeInterval = 24, -- em horas
-	minimumLevel = 70,
-	membersNeeded = 4,
-	checkDifferentIps = true,
-	minimumDifferentIps = 4,
-	pointAmount = 10
+	minimumLevel = 8,
+	membersNeeded = 2,
+	checkDifferentIps = false,
+	minimumDifferentIps = 2,
+	pointAmount = 10,
 	accountStorage = 9999
 }
 
@@ -53,28 +51,32 @@ function onSay(player, words, param)
 
 	local membersTable, validIpsTable = {}, {}
 	for _, member in ipairs(guild:getMembersOnline()) do
-		if member:getLevel() >= config.minimumLevel then
-			local accountId = member:getAccountId()
-			local getAccountStorage = Game.getAccountStorageValue(accountId, config.accountStorage)
-			if not getAccountStorage then
-				player:getPosition():sendMagicEffect(CONST_ME_POFF)
-				return false
-			end
-
-			if getAccountStorage < 1 then
-				table.insert(membersTable, member:getGuid())
-				local ipCount = 0
-				local ip = member:getIp()
-				for i = 1, #validIpsTable do
-					if validIpsTable[i] then
-						if ip == validIpsTable[i] then
-							ipCount = ipCount + 1
-						end
-					end
+		if member then
+			if member:getLevel() >= config.minimumLevel then
+				local accountId = member:getAccountId()
+				local getAccountStorage = Game.getAccountStorageValue(accountId, config.accountStorage)
+				if not getAccountStorage then
+					player:getPosition():sendMagicEffect(CONST_ME_POFF)
+					return false
 				end
 
-				if ipCount == 0 and checkDifferentIps then
-					table.insert(validIpsTable, ip)
+				if getAccountStorage < 1 then
+					table.insert(membersTable, member:getGuid())
+					local ipCount = 0
+					local ip = member:getIp()
+					if ip > 0 then
+						for i = 1, #validIpsTable do
+							if validIpsTable[i] then
+								if ip == validIpsTable[i] then
+									ipCount = ipCount + 1
+								end
+							end
+						end
+
+						if ipCount == 0 and config.checkDifferentIps then
+							table.insert(validIpsTable, ip)
+						end
+					end
 				end
 			end
 		end
@@ -87,23 +89,30 @@ function onSay(player, words, param)
 		return false
 	end
 
-	local totalMembrosIps = #validIpsTable
-	if totalMembrosIps < config.minimumDifferentIps then
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_RED, '[GUILD POINTS] Only ' .. totalMembrosIps .. ' members are valid, you need ' ..config.minimumDifferentIps .. ' players with different ip addresses.')
-		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		return false
+	if config.checkDifferentIps then
+		local totalMembrosIps = #validIpsTable
+		if totalMembrosIps < config.minimumDifferentIps then
+			player:sendTextMessage(MESSAGE_STATUS_CONSOLE_RED, '[GUILD POINTS] Only ' .. totalMembrosIps .. ' members are valid, you need ' ..config.minimumDifferentIps .. ' players with different ip addresses.')
+			player:getPosition():sendMagicEffect(CONST_ME_POFF)
+			return false
+		end
 	end
 
 	db.query('UPDATE `guilds` SET `last_execute_points` = ' .. (os.time() + config.executeInterval * 3600) .. ' WHERE `guilds`.`id` = '.. guildId ..';')
 
-	for _, member in ipairs(membersTable) do
-		local accountId = member:getAccountId()
-		Game.setAccountStorageValue(accountId, config.accountStorage, 1)
-		db.query('UPDATE `znote_accounts` SET `guild_points` = `guild_points` + ' .. config.pointAmount .. ', `guild_points_stats` = ' .. os.time() .. ' WHERE `account_id` = '.. accountId)
-		member:sendTextMessage(MESSAGE_INFO_DESCR, 'You received ' .. config.pointAmount .. ' guild points.')
+	for i = 1, #membersTable do
+		if membersTable[i] then
+			local member = Player(membersTable[i])
+			if member then
+				local accountId = member:getAccountId()
+				Game.setAccountStorageValue(accountId, config.accountStorage, 1)
+				db.query('UPDATE `znote_accounts` SET `guild_points` = `guild_points` + ' .. config.pointAmount .. ' WHERE `account_id` = '.. accountId)
+				member:sendTextMessage(MESSAGE_INFO_DESCR, '[GUILD POINTS] You received ' .. config.pointAmount .. ' guild points.')
+			end
+		end
 	end
 
-	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, #membersTable .. ' guild members received points.')
+	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_ORANGE, '[GUILD POINTS] '.. #membersTable .. ' guild members received points.')
 
 	return false
 end

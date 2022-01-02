aa95342dcde0100b139c539dc29d68bca0081a22
Luan Luciano <luanluciano@outlook.com>
2022-01-01 23:42:39 -0300

    Update guildshop.php

--- LUA/guildshop/guildshop.php
+++ LUA/guildshop/guildshop.php
@@ -12,6 +12,10 @@ $loggedin = user_logged_in();
 
 $shop_list = $config['guild_shop_offers'];
 
+if ($shop['enabled']) {
+?>
+<div class="title"> <h1> Guild Shop Offers</h1> </div>
+<?php
 if ($loggedin === true) {
 	if (!empty($_POST['buy']) && $_SESSION['shop_session'] == $_POST['session']) {
 		$time = time();
@@ -26,7 +30,7 @@ if ($loggedin === true) {
 				$buy = $value;
 			}
 		}
-		if ($buy === false) die("Error: Guild Shop offer ID mismatch.");
+		if ($buy === false) die("Error: Guild shop offer ID mismatch.");
 
 		// Verify that user can afford this offer.
 		if ($player_points >= $buy['guild_points']) {
@@ -61,44 +65,57 @@ if ($loggedin === true) {
 			} else if ($buy['type'] == 3) {
 				// Character Gender
 				mysql_insert("INSERT INTO `znote_shop_orders` (`account_id`, `type`, `itemid`, `count`, `time`) VALUES ('$cid', '". $buy['type'] ."', '". $buy['itemid'] ."', '". $buy['count'] ."', '$time')");
-				echo '<font color="green" size="4">You now have access to change character gender on your characters. Visit <a href="myaccount.php">My Account</a> to select character and change the gender.</font>';
+				echo '<p><font color="green" size="4">You now have access to change character gender on your characters. Visit <a href="myaccount.php">My Account</a> to select character and change the gender.</font></p>';
 			} else if ($buy['type'] == 4) {
 				// Character Name
 				mysql_insert("INSERT INTO `znote_shop_orders` (`account_id`, `type`, `itemid`, `count`, `time`) VALUES ('$cid', '". $buy['type'] ."', '". $buy['itemid'] ."', '". $buy['count'] ."', '$time')");
-				echo '<font color="green" size="4">You now have access to change character name on your characters. Visit <a href="myaccount.php">My Account</a> to select character and change the name.</font>';
+				echo '<p><font color="green" size="4">You now have access to change character name on your characters. Visit <a href="myaccount.php">My Account</a> to select character and change the name.</font></p>';
 			} else {
 				mysql_insert("INSERT INTO `znote_shop_orders` (`account_id`, `type`, `itemid`, `count`, `time`) VALUES ('$cid', '". $buy['type'] ."', '". $buy['itemid'] ."', '". $buy['count'] ."', '$time')");
-				echo '<font color="green" size="4">Your order is ready to be delivered. Write this command in-game to get it: [!shop].<br>Make sure you are in depot and can carry it before executing the command!</font>';
+				echo '<p><font color="green" size="4">Your order is ready to be delivered. Write this command in-game to get it: [!shop].<br>Make sure you are in depot and can carry it before executing the command!</font></p>';
 			}
 
 			// No matter which type, we will always log it.
-			mysql_insert("INSERT INTO `znote_shop_logs` (`account_id`, `player_id`, `type`, `itemid`, `count`, `points`, `time`) VALUES ('$cid', '0', '". $buy['type'] ."', '". $buy['itemid'] ."', '". $buy['count'] ."', '". $buy['points'] ."', '$time')");
+			mysql_insert("INSERT INTO `znote_shop_logs` (`account_id`, `player_id`, `type`, `itemid`, `count`, `points`, `time`) VALUES ('$cid', '0', '". $buy['type'] ."', '". $buy['itemid'] ."', '". $buy['count'] ."', '". $buy['guild_points'] ."', '$time')");
 
-		} else echo '<font color="red" size="4">You need more guild points, this offer cost '.$buy['guild_points'].' guild points.</font>';
+		} else echo '<p><font color="red" size="4">You need more guild points, this offer cost '.$buy['guild_points'].' points.</font></p>';
 		//var_dump($buy);
 		//echo '<font color="red" size="4">'. $_POST['buy'] .'</font>';
 	}
 }
 
-if ($shop['enabled']) {
-?>
-
-<h1>Guild Shop Offers</h1>
-<?php
 if ($loggedin === true) {
 	if (!empty($_POST['buy']) && $_SESSION['shop_session'] == $_POST['session']) {
 		if ($user_znote_data['guild_points'] >= $buy['guild_points']) {
-			?><td>You have <?php echo (int)($user_znote_data['guild_points'] - $buy['guild_points']); ?> guild points.</td><?php
+			?><p><b><font color="red">You have <?php echo (int)($user_znote_data['guild_points'] - $buy['guild_points']); ?> guild points.</font></b></p><?php
 		} else {
-			?><td>You have <?php echo $user_znote_data['guild_points']; ?> guild points.</td><?php
+			?><p><b><font color="red">You have <?php echo $user_znote_data['guild_points']; ?> guild points.</font></b></p><?php
 		}
 	} else {
-		?><td>You have <?php echo $user_znote_data['guild_points']; ?> guild points.</td><?php
+		?><p><b><font color="red">You have <?php echo $user_znote_data['guild_points']; ?> guild points.</font></b></p><?php
 	}
 } else {
 	?><p>You need to be logged in to use the guild shop.</p><?php
 }
 
+?>
+<table class="table tbl-hover">
+	<thead>
+		<tr class="yellow">
+			<td colspan="2"><center>Guild Shop System</center></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr> <td> <center>Comando Utilizado:</center> </td> <td> <b> !guildpoints (leader)</b> </td> </tr>
+		<tr> <td> <center>Level Mínimo:</center> </td> <td> <b>	Level 80+</b> </td> </tr>
+		<tr> <td> <center>Players Online:</center> </td> <td> <b> 4 Online</b> </td> </tr>
+		<tr> <td> <center>IPs Diferentes:</center> </td> <td> <b>	4 IPs</b> </td> </tr>
+		<tr> <td> <center>Quantidade de Pontos:</center> </td> <td> <b>	10 Pontos (cada player)</b> </td> </tr>
+		<tr> <td> <center>Obs:</center> </td> <td> <b> O benefício só poderá ser adquirido uma vez por account. </b> </td> </tr>
+	</tbody>
+</table>
+
+<?php
 $outfitsIds = array(136,137,138,139,140,141,142,147,148,149,150,155,156,157,158,252,269,270,279,288,324,336,366,431,433,464,466,471,513,514,542,128,129,130,131,132,133,134,143,144,145,146,151,152,153,154,251,268,273,278,289,325,335,367,430,432,463,465,472,512,516,541);
 $category_items = array();
 $category_premium = array();
@@ -136,11 +153,11 @@ foreach ($shop_list as $key => $offer) {
 ?>
 <div id="categoryNavigator">
 	<a class="nav_link" href="#all">ALL</a>
-	<?php if (!empty($category_items)): ?><a class="nav_link" href="#cat_itemids">ITEMS</a><?php endif; ?>
 	<?php if (!empty($category_premium)): ?><a class="nav_link" href="#cat_premium">PREMIUM</a><?php endif; ?>
+	<?php if (!empty($category_misc)): ?><a class="nav_link" href="#cat_misc">MISC</a><?php endif; ?>
+	<?php if (!empty($category_items)): ?><a class="nav_link" href="#cat_itemids">ITEMS</a><?php endif; ?>
 	<?php if (!empty($category_outfits)): ?><a class="nav_link" href="#cat_outfits">OUTFITS</a><?php endif; ?>
 	<?php if (!empty($category_mounts)): ?><a class="nav_link" href="#cat_mounts">MOUNTS</a><?php endif; ?>
-	<?php if (!empty($category_misc)): ?><a class="nav_link" href="#cat_misc">MISC</a><?php endif; ?>
 </div>
 <script type="text/javascript">
 	function domReady () {
@@ -201,37 +218,7 @@ foreach ($shop_list as $key => $offer) {
 	}
 </script>
 
-<?php if (!empty($category_items)): ?>
-	<!-- ITEMIDS -->
-	<table class="show" id="cat_itemids">
-		<tr class="yellow">
-			<td>Item:</td>
-			<?php if ($config['guild_shop']['showImage']) { ?><td>Image:</td><?php } ?>
-			<td>Count:</td>
-			<td>Guild Points:</td>
-			<?php if ($loggedin === true): ?><td>Action:</td><?php endif; ?>
-		</tr>
-		<?php foreach ($category_items as $key => $offers): ?>
-			<tr class="special">
-				<td><?php echo $offers['description']; ?></td>
-				<?php if ($config['guild_shop']['showImage']):?>
-					<td><img src="http://<?php echo $config['guild_shop']['imageServer']; ?>/<?php echo $offers['itemid']; ?>.<?php echo $config['guild_shop']['imageType']; ?>" alt="img"></td>
-				<?php endif; ?>
-				<td><?php echo $offers['count']; ?>x</td>
-				<td><?php echo $offers['guild_points']; ?></td>
-				<?php if ($loggedin === true): ?>
-				<td>
-					<form action="" method="POST">
-						<input type="hidden" name="buy" value="<?php echo (int)$key; ?>">
-						<input type="hidden" name="session" value="<?php echo time(); ?>">
-						<input type="submit" value="  PURCHASE  "  class="needconfirmation" data-item-name="<?php echo $offers['description']; ?>" data-item-cost="<?php echo $offers['guild_points']; ?>">
-					</form>
-				</td>
-				<?php endif; ?>
-			</tr>
-		<?php endforeach; ?>
-	</table>
-<?php endif; ?>
+
 <?php if (!empty($category_premium)): ?>
 <!-- PREMIUM DURATION -->
 <table class="show" id="cat_premium">
@@ -246,7 +233,7 @@ foreach ($shop_list as $key => $offer) {
 		<tr class="special">
 			<td><?php echo $offers['description']; ?></td>
 			<?php if ($config['guild_shop']['showImage']):?>
-				<td><img src="http://<?php echo $config['guild_shop']['imageServer']; ?>/<?php echo $offers['itemid']; ?>.<?php echo $config['guild_shop']['imageType']; ?>" alt="img"></td>
+				<td><img src="images/shop/premium.gif" alt="premium scroll"></td>
 			<?php endif; ?>
 			<td><?php echo $offers['count']; ?> Days</td>
 			<td><?php echo $offers['guild_points']; ?></td>
@@ -262,6 +249,72 @@ foreach ($shop_list as $key => $offer) {
 		</tr>
 	<?php endforeach; ?>
 </table>
+<?php endif; ?>
+<?php if (!empty($category_misc)): ?>
+<!-- MISCELLANEOUS -->
+<table class="show" id="cat_misc">
+	<tr class="yellow">
+		<td>Description:</td>
+		<?php if ($config['guild_shop']['showImage']) { ?><td>Image:</td><?php } ?>
+		<td>Count/duration:</td>
+		<td>Guild Points:</td>
+		<?php if ($loggedin === true): ?><td>Action:</td><?php endif; ?>
+	</tr>
+	<?php foreach ($category_misc as $key => $offers): ?>
+		<tr class="special">
+			<td><?php echo $offers['description']; ?></td>
+			<?php if ($config['guild_shop']['showImage']):?>
+				<td><img src="images/shop/<?php echo $offers['itemid']; ?>.gif" alt="<?php echo $offers['description']; ?>"></td>
+			<?php endif;
+			if ($offers['count'] === 0): ?>
+				<td>Unlimited</td>
+			<?php else: ?>
+				<td><?php echo $offers['count']; ?>x</td>
+			<?php endif; ?>
+			<td><?php echo $offers['guild_points']; ?></td>
+			<?php if ($loggedin === true): ?>
+			<td>
+				<form action="" method="POST">
+					<input type="hidden" name="buy" value="<?php echo (int)$key; ?>">
+					<input type="hidden" name="session" value="<?php echo time(); ?>">
+					<input type="submit" value="  PURCHASE  "  class="needconfirmation" data-item-name="<?php echo $offers['description']; ?>" data-item-cost="<?php echo $offers['guild_points']; ?>">
+				</form>
+			</td>
+			<?php endif; ?>
+		</tr>
+	<?php endforeach; ?>
+</table>
+<?php endif; ?>
+<?php if (!empty($category_items)): ?>
+	<!-- ITEMIDS -->
+	<table class="show" id="cat_itemids">
+		<tr class="yellow">
+			<td>Item:</td>
+			<?php if ($config['guild_shop']['showImage']) { ?><td>Image:</td><?php } ?>
+			<td>Count:</td>
+			<td>Guild Points:</td>
+			<?php if ($loggedin === true): ?><td>Action:</td><?php endif; ?>
+		</tr>
+		<?php foreach ($category_items as $key => $offers): ?>
+			<tr class="special">
+				<td><?php echo $offers['description']; ?></td>
+				<?php if ($config['guild_shop']['showImage']):?>
+					<td><img src="images/shop/<?php echo $offers['itemid']; ?>.gif" alt="<?php echo $offers['description']; ?>"></td>
+				<?php endif; ?>
+				<td><?php echo $offers['count']; ?>x</td>
+				<td><?php echo $offers['guild_points']; ?></td>
+				<?php if ($loggedin === true): ?>
+				<td>
+					<form action="" method="POST">
+						<input type="hidden" name="buy" value="<?php echo (int)$key; ?>">
+						<input type="hidden" name="session" value="<?php echo time(); ?>">
+						<input type="submit" value="  PURCHASE  "  class="needconfirmation" data-item-name="<?php echo $offers['description']; ?>" data-item-cost="<?php echo $offers['guild_points']; ?>">
+					</form>
+				</td>
+				<?php endif; ?>
+			</tr>
+		<?php endforeach; ?>
+	</table>
 <?php endif; ?>
 <?php if (!empty($category_outfits)): ?>
 <!-- OUTFITS -->
@@ -286,7 +339,7 @@ foreach ($shop_list as $key => $offer) {
 			<td><?php echo $offers['description']; ?></td>
 			<?php if ($config['show_outfits']['shop']):?>
 				<td><?php foreach($offers['itemid'] as $outfitId): ?>
-					<img src="<?php echo $config['show_outfits']['imageServer']; ?>?id=<?php echo $outfitId; ?>&addons=<?php echo $offers['count']; ?>&head=<?php echo rand(1, 132); ?>&body=<?php echo rand(1, 132); ?>&legs=<?php echo rand(1, 132); ?>&feet=<?php echo rand(1, 132); ?>" alt="img">
+					<img src="images/shop/<?php echo $outfitId; ?>.gif" alt="outfit <?php echo $outfitId; ?>">
 				<?php endforeach; ?></td>
 			<?php endif; ?>
 			<td><?php echo $offers['guild_points']; ?></td>
@@ -332,41 +385,6 @@ foreach ($shop_list as $key => $offer) {
 	<?php endforeach; ?>
 </table>
 <?php endif; ?>
-<?php if (!empty($category_misc)): ?>
-<!-- MISCELLANEOUS -->
-<table class="show" id="cat_misc">
-	<tr class="yellow">
-		<td>Description:</td>
-		<?php if ($config['guild_shop']['showImage']) { ?><td>Image:</td><?php } ?>
-		<td>Count/duration:</td>
-		<td>Guild Points:</td>
-		<?php if ($loggedin === true): ?><td>Action:</td><?php endif; ?>
-	</tr>
-	<?php foreach ($category_misc as $key => $offers): ?>
-		<tr class="special">
-			<td><?php echo $offers['description']; ?></td>
-			<?php if ($config['guild_shop']['showImage']):?>
-				<td><img src="http://<?php echo $config['guild_shop']['imageServer']; ?>/<?php echo $offers['itemid']; ?>.<?php echo $config['guild_shop']['imageType']; ?>" alt="img"></td>
-			<?php endif;
-			if ($offers['count'] === 0): ?>
-				<td>Unlimited</td>
-			<?php else: ?>
-				<td><?php echo $offers['count']; ?>x</td>
-			<?php endif; ?>
-			<td><?php echo $offers['guild_points']; ?></td>
-			<?php if ($loggedin === true): ?>
-			<td>
-				<form action="" method="POST">
-					<input type="hidden" name="buy" value="<?php echo (int)$key; ?>">
-					<input type="hidden" name="session" value="<?php echo time(); ?>">
-					<input type="submit" value="  PURCHASE  "  class="needconfirmation" data-item-name="<?php echo $offers['description']; ?>" data-item-cost="<?php echo $offers['guild_points']; ?>">
-				</form>
-			</td>
-			<?php endif; ?>
-		</tr>
-	<?php endforeach; ?>
-</table>
-<?php endif; ?>
 
 <?php if ($shop['enableShopConfirmation']) { ?>
 <script src="https://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
@@ -389,5 +407,5 @@ foreach ($shop_list as $key => $offer) {
 	// Store current timestamp to prevent page-reload from processing old purchase
 	$_SESSION['shop_session'] = time();
 
-} else echo '<h1>Buy Guild Points system disabled.</h1><p>Sorry, this functionality is disabled.</p>';
+} else echo '<div class="title"> <h1> Guild Shop disabled </h1> </div> <p>Sorry, this functionality is disabled.</p>';
 include 'layout/overall/footer.php'; ?>

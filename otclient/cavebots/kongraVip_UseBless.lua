201481eb89ba6bd947bcea65d8625c39d6358f50
Luan Luciano <luanluciano@outlook.com>
2022-09-15 23:53:51 -0300

    Create kongraVip_UseBless.lua

---
+++ otclient/cavebots/kongraVip_UseBless.lua
@@ -0,0 +1,47 @@
+say:!bless
+
+goto:1433,528,7
+goto:1436,534,7
+goto:1430,535,7
+goto:1424,535,7
+goto:1423,541,7
+goto:1422,547,7
+goto:1428,547,7
+goto:1434,546,7
+goto:1435,552,7
+goto:1435,558,7
+goto:1433,564,7
+goto:1429,570,7
+goto:1435,574,7
+goto:1440,580,7
+goto:1446,579,7
+goto:1440,581,7
+goto:1434,584,7
+goto:1432,590,7
+goto:1426,589,7
+goto:1420,589,7
+goto:1414,584,7
+goto:1418,578,7
+goto:1413,572,7
+goto:1410,566,7
+goto:1427,565,8
+
+say:exani tera
+say:exani tera
+
+goto:1416,570,7
+goto:1416,576,7
+goto:1416,570,7
+goto:1416,564,7
+goto:1415,558,7
+goto:1415,552,7
+goto:1418,546,7
+goto:1416,540,7
+goto:1417,534,7
+goto:1422,528,7
+goto:1416,527,7
+goto:1422,525,7
+goto:1428,522,7
+
+config:{"useDelay":400,"mapClickDelay":100,"walkDelay":10,"ping":100,"ignoreFields":true,"skipBlocked":true,"mapClick":true}
+extensions:[[{"Depositer": [], "Supply": []}]]
